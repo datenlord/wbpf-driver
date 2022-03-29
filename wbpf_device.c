059a69fd61b4f2b0a7aefd566986305717b7260f
losfair <zhy20000919@hotmail.com>
2022-03-29 04:35:20 +0000

    Tweak

--- wbpf_device.c
+++ wbpf_device.c
@@ -8,10 +8,7 @@ int wbpf_device_probe(struct wbpf_device *wdev)
   uint32_t hw_revision;
   uint64_t start_time, end_time;
 
-  pe_info = readl(mmio_base_for_core(wdev, 0) + 0x30);
-  wdev->num_pe = pe_info >> 16;
-  pr_info("wbpf: number of processing elements: %d\n",
-          wdev->num_pe);
+  // Read HW rev
   hw_revision = readl(mmio_base_for_core(wdev, 0) + 0x34);
   wdev->hw_revision_major = hw_revision >> 16;
   wdev->hw_revision_minor = hw_revision & 0xffff;
@@ -21,8 +18,14 @@ int wbpf_device_probe(struct wbpf_device *wdev)
            wdev->hw_revision_major, wdev->hw_revision_minor);
     return -ENODEV;
   }
-  pr_info("wbpf: hardware revision: %u.%u\n",
-          wdev->hw_revision_major, wdev->hw_revision_minor);
+
+  // Read PE info
+  pe_info = readl(mmio_base_for_core(wdev, 0) + 0x30);
+  wdev->num_pe = pe_info >> 16;
+
+  pr_info("wbpf: hardware revision: %u.%u, number of processing elements: %d\n",
+          wdev->hw_revision_major, wdev->hw_revision_minor,
+          wdev->num_pe);
 
   // At this point we haven't enabled IRQ yet.
   start_time = ktime_to_ns(ktime_get());
